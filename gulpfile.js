7fe5b47cddc95e2eda71728e4089a46dcbb8dea8
Raudel <gomezsmith.raudel@gamil.com>
2021-08-23 15:29:29 +0200

    salva-todo-funcionando-correcto-menos-gulp-watch-mas-de-una-vez

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var runSequence = require('gulp4-run-sequence');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
+
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
@@ -14,19 +16,11 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function() {
+
   return gulp.src('./app/assets/styles/styles.css')
-	  .pipe(postcss([cssvars, nested, autoprefixer]))
+	  .pipe(postcss([cssvars({ silent: true }), nested, autoprefixer]))
 	  .pipe(gulp.dest('./app/temp/styles'));
 });
 
-gulp.task('watch', function() {
 
-  watch('./app/index.html', function() {
-    gulp.start('html');
-  });
-
-  watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
-  });
-
-});
+gulp.task('watch', gulp.parallel('html','styles'));
