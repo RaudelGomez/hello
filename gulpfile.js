f81f666ce1a058cf7fdbf65f1946212d2f212c43
Raudel <gomezsmith.raudel@gamil.com>
2021-08-26 17:43:33 +0200

    termine-hasta-parte-2-sprites

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,39 @@ var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
 var mixins = require('postcss-mixins');
 var browserSync = require('browser-sync').create(); 
+var svgSprite = require('gulp-svg-sprite');
+var rename = require('gulp-rename');
+
+
+//Tarea Sprite
+
+var config = {
+  mode: {
+    css: {
+      render: {
+        css: {
+          template: './app/templates/sprite.css'
+        }
+      }
+    }
+  }
+}
+
+
+
+gulp.task('createSprite', function() {
+  return gulp.src('./app/assets/images/icons/**/*.svg')
+    .pipe(svgSprite(config)) 
+    .pipe(gulp.dest('./app/temp/sprite'));
+});
+
+gulp.task('copySpriteCSS', function() {
+  return gulp.src('./app/temp/sprite/css/*.css')
+  .pipe(rename('_sprite.css')) 
+  .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons', gulp.series(['createSprite', 'copySpriteCSS']));
 
 //Tarea Styles
 gulp.task('styles', function() {
