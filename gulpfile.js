f3ce099ee3bec09cbaa5814637547bfe6b3cb227
Raudel <gomezsmith.raudel@gamil.com>
2021-08-31 00:58:00 +0200

    empezando con babel

--- gulpfile.js
+++ gulpfile.js
@@ -15,8 +15,11 @@ var webpack = require('webpack');
 
 //Tarea iniciar webpack
 gulp.task('scripts', function(callback) {
-  webpack(require('./webpack.config.js'), function() {
-    console.log("webpack cargado");
+  webpack(require('./webpack.config.js'), function(err, stats) {
+    if (err) {
+      console.log(err.toString());
+    }
+    console.log(stats.toString());
     callback();
   });
 });
